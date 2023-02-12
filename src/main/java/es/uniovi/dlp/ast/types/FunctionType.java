a12a2cd192bb579767a3c294f39dd2528050caf9
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-02-12 18:19:20 +0100

    Soluciono TODO debido a clase no definida

--- src/main/java/es/uniovi/dlp/ast/types/FunctionType.java
+++ src/main/java/es/uniovi/dlp/ast/types/FunctionType.java
@@ -1,7 +1,11 @@
 package es.uniovi.dlp.ast.types;
 
+import es.uniovi.dlp.ast.program.VarDefinition;
+
+import java.util.List;
+
 public class FunctionType extends AbstractType {
-    public Object params; //TODO clase VarDefinition
+    public List<VarDefinition> params;
     public Type returnType;
 
     public FunctionType(int line, int column) {
