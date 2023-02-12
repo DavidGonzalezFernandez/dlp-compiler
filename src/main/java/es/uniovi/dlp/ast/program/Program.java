2df06a021414a70b2bee8fb90b14828d25b65613
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-02-12 13:52:27 +0100

    Esqueleto básico de Program

---
+++ src/main/java/es/uniovi/dlp/ast/program/Program.java
@@ -0,0 +1,14 @@
+package es.uniovi.dlp.ast.program;
+
+import es.uniovi.dlp.ast.AbstractASTNode;
+
+import java.util.List;
+
+// El programa es un nodo porque es el nodo más alto en el árbol
+public class Program extends AbstractASTNode {
+    private List<Definition> definitions;
+
+    public Program(int line, int column) {
+        super(line, column);
+    }
+}
