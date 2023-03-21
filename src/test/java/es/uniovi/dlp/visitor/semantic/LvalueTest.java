cc61827e6f87a78807f161a61fbd70f72e748081
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-03-21 13:12:47 +0100

    Añado test propio

--- src/test/java/es/uniovi/dlp/visitor/semantic/LvalueTest.java
+++ src/test/java/es/uniovi/dlp/visitor/semantic/LvalueTest.java
@@ -31,6 +31,44 @@ public class LvalueTest {
     assertFoundErrors(new ArrayList<>());
   }
 
+  // ---
+
+  @Test
+  void errorTestPropio() {
+    runCompiler("examples/errors/lvalue/test_propio.xana");
+    assertFoundErrors(
+      Arrays.asList(
+          new Error(56, 5, ErrorReason.LVALUE_REQUIRED),
+          new Error(57, 5, ErrorReason.LVALUE_REQUIRED),
+          new Error(58, 5, ErrorReason.LVALUE_REQUIRED),
+          new Error(59, 5, ErrorReason.LVALUE_REQUIRED),
+          new Error(60, 5, ErrorReason.LVALUE_REQUIRED),
+          new Error(61, 5, ErrorReason.LVALUE_REQUIRED),
+          new Error(62, 5, ErrorReason.LVALUE_REQUIRED),
+          new Error(63, 5, ErrorReason.LVALUE_REQUIRED),
+          new Error(64, 5, ErrorReason.LVALUE_REQUIRED),
+          new Error(65, 5, ErrorReason.LVALUE_REQUIRED),
+          new Error(66, 5, ErrorReason.LVALUE_REQUIRED),
+          new Error(67, 5, ErrorReason.LVALUE_REQUIRED),
+
+          new Error(70, 8, ErrorReason.LVALUE_REQUIRED),
+          new Error(70, 25, ErrorReason.LVALUE_REQUIRED),
+          new Error(70, 42, ErrorReason.LVALUE_REQUIRED),
+          new Error(70, 59, ErrorReason.LVALUE_REQUIRED),
+          new Error(71, 8, ErrorReason.LVALUE_REQUIRED),
+          new Error(71, 26, ErrorReason.LVALUE_REQUIRED),
+          new Error(71, 44, ErrorReason.LVALUE_REQUIRED),
+          new Error(71, 49, ErrorReason.LVALUE_REQUIRED),
+          new Error(71, 54, ErrorReason.LVALUE_REQUIRED),
+          new Error(72, 8, ErrorReason.LVALUE_REQUIRED),
+          new Error(72, 30, ErrorReason.LVALUE_REQUIRED),
+          new Error(72, 39, ErrorReason.LVALUE_REQUIRED)
+        )
+    );
+  }
+
+  // ---
+
   private static void assertFoundErrors(List<Error> errors) {
     var foundErrors = ErrorManager.getInstance().getErrors();
     assertIterableEquals(errors, foundErrors);
